3248b0ffeec75fa860f86afe4d64ce8a72034f4b
Roguez84 <t.roguez984@gmail.com>
2021-04-07 17:16:28 -0400

    Created appointment tables which is intermediate to the other 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_210246) do
+ActiveRecord::Schema.define(version: 2021_04_07_211032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "date_time", null: false
+    t.integer "cost"
+    t.bigint "patient_id"
+    t.bigint "doctor_id"
+    t.bigint "invoice_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["invoice_id"], name: "index_appointments_on_invoice_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string "RUT", null: false
@@ -44,4 +57,7 @@ ActiveRecord::Schema.define(version: 2021_04_07_210246) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "appointments", "doctors"
+  add_foreign_key "appointments", "invoices"
+  add_foreign_key "appointments", "patients"
 end
