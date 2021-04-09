47ac32668ad57a0d0174e85fdb9fe57055ef4dce
Roguez84 <t.roguez984@gmail.com>
2021-04-09 13:46:04 -0400

    Made necessary changes to have devise on a second model (Patient)

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :patients
+  #devise_for :doctors
+  #devise_for :patients
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'patients/index'
   get 'doctors/index'
   get 'home/index'
-  #devise_for :doctors
   get 'appointments/index'
   root 'home#index'
   devise_for :doctors, controllers: {
         sessions: 'doctors/sessions', registrations: 'doctors/registrations'
+      }
+  devise_for :patients, controllers: {
+        sessions: 'patients/sessions', registrations: 'patients/registrations'
       }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
