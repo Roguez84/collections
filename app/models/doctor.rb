711dc61a1597c605917706c6f6f0bcc2fba59124
Roguez84 <t.roguez984@gmail.com>
2021-04-12 15:08:40 -0400

    added sign in with google (pending create new user with google

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,7 +3,22 @@ class Doctor < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :appointments
   has_many :invoices, through: :appointments
   has_many :patients, through: :appointments
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    doctor = Doctor.where(email: data.email).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless doctor
+        doctor = Doctor.create(names: data.name,
+            RUT: "9999-k",
+           email: data.email,
+           password: Devise.friendly_token[0,20]
+        )
+    end
+    doctor
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,11 +21,13 @@ Rails.application.routes.draw do
   #devise_for :doctors
   #devise_for :patients
   devise_for :doctors, controllers: {
-        sessions: 'doctors/sessions', registrations: 'doctors/registrations'
+        sessions: 'doctors/sessions', registrations: 'doctors/registrations', omniauth_callbacks: 'doctors/omniauth_callbacks'
       }
+  #devise_for :doctors, controllers: { omniauth_callbacks: 'doctors/omniauth_callbacks' }
   devise_for :patients, controllers: {
         sessions: 'patients/sessions', registrations: 'patients/registrations'
       }
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
